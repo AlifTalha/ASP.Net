3570b9df733250dd47a0aa014cfa28363913a114
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-10-26 01:43:17 +0600

    Add files via upload

---
+++ MID/Task1/Controllers/BioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BioController : Controller
+    {
+        public ActionResult Bio()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+    }
+}
