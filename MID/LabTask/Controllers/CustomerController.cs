81e185d7fe5c40f7f34f16d79c585f14d0afe335
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-11-13 23:12:03 +0600

    Add files via upload

---
+++ MID/LabTask/Controllers/CustomerController.cs
@@ -0,0 +1,176 @@
+﻿using LabTask.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabTask.DTOs;
+
+
+namespace LabTask.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerDBEntities db = new CustomerDBEntities();
+
+        public static Customer Convert(CustomerDTO c)
+        {
+            return new Customer
+            {
+                CustomerId = c.CustomerId,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Email = c.Email,
+                Phone = int.Parse(c.Phone),
+                Address = c.Address,
+                DateJoined = c.DateJoined
+            };
+        }
+
+        public static CustomerDTO Convert(Customer c)
+        {
+            return new CustomerDTO
+            {
+                CustomerId = c.CustomerId,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Email = c.Email,
+                Phone = c.Phone.ToString(), 
+                Address = c.Address,
+                DateJoined = c.DateJoined
+            };
+        }
+
+
+        public static List<CustomerDTO> Convert(List<Customer> data)
+        {
+            var list = new List<CustomerDTO>();
+            foreach (var c in data)
+            {
+                list.Add(Convert(c));
+            }
+            return list;
+        }
+
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            var data = db.Customers.ToList();
+            return View(Convert(data));
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new CustomerDTO());
+
+        }
+        [HttpPost]
+        public ActionResult Create(CustomerDTO c)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(Convert(c));
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var exobj = db.Customers.Find(Id);
+
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(Convert(exobj)); 
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var exobj = db.Customers.Find(Id);
+
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(Convert(exobj)); 
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(CustomerDTO c)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(c);
+            }
+            var exobj = db.Customers.Find(c.CustomerId);
+
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+
+            exobj.FirstName = c.FirstName;
+            exobj.LastName = c.LastName;
+            exobj.Email = c.Email;
+
+            if (int.TryParse(c.Phone, out int phone))
+            {
+                exobj.Phone = phone;
+            }
+            else
+            {
+                ModelState.AddModelError("Phone", "Invalid phone number format.");
+                return View(c); 
+            }
+
+            exobj.Address = c.Address;
+            exobj.DateJoined = c.DateJoined;
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var exobj = db.Customers.Find(Id);
+
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(Convert(exobj));
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int Id)
+        {
+            var exobj = db.Customers.Find(Id);
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+            db.Customers.Remove(exobj);
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+
+
+
+    }
+}
---
+++ MID/LabTask/Controllers/ProductController.cs
@@ -0,0 +1,155 @@
+﻿using LabTask.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabTask.DTOs;
+
+namespace LabTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        CustomerDBEntities db = new CustomerDBEntities();
+
+        public static Product Convert(ProductDTO p)
+        {
+            return new Product
+            {
+                ProductId = p.ProductId,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                StockQuantity = p.StockQuantity,
+                Category = p.Category
+            };
+        }
+
+        public static ProductDTO Convert(Product p)
+        {
+            return new ProductDTO
+            {
+                ProductId = p.ProductId,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                StockQuantity = p.StockQuantity,
+                Category = p.Category
+            };
+        }
+
+        public static List<ProductDTO> Convert(List<Product> data)
+        {
+            var list = new List<ProductDTO>();
+            foreach (var p in data)
+            {
+                list.Add(Convert(p));
+            }
+            return list;
+        }
+
+        
+        public ActionResult List()
+        {
+            var data = db.Products.ToList();
+            return View(Convert(data));
+        }
+
+
+        public ActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(new List<string> { "Electronics", "Clothing", "Sports", "Tech" });
+            return View(new ProductDTO());
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductDTO p)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = new SelectList(new List<string> { "Electronics", "Clothing", "Sports", "Tech" });
+                return View(p);
+            }
+
+            db.Products.Add(Convert(p));
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+
+        public ActionResult Details(int Id)
+        {
+            var exobj = db.Products.Find(Id);
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Convert(exobj));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var exobj = db.Products.Find(Id);
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Categories = new SelectList(new List<string> { "Sports", "Tech", "Clothing" }, exobj.Category);
+
+            return View(Convert(exobj));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductDTO p)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = new SelectList(new List<string> { "Sports", "Tech", "Clothing" }, p.Category);
+                return View(p);
+            }
+
+            var exobj = db.Products.Find(p.ProductId);
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+
+            exobj.Name = p.Name;
+            exobj.Description = p.Description;
+            exobj.Price = p.Price;
+            exobj.StockQuantity = p.StockQuantity;
+            exobj.Category = p.Category;
+
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var exobj = db.Products.Find(Id);
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Convert(exobj));
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int Id)
+        {
+            var exobj = db.Products.Find(Id);
+            if (exobj == null)
+            {
+                return HttpNotFound();
+            }
+            db.Products.Remove(exobj);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
