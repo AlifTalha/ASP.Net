ba0bf8b22381a227c7204ec30226cd04ecc2538e
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-12-22 01:20:50 +0600

    Add files via upload

---
+++ Final/FinalTask1/Models/TokenModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalTaskLab1.Models
+{
+    using System;
+
+    public class Token
+    {
+        public int Id { get; set; }
+        public string TokenNumber { get; set; }
+        public string CounterType { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
