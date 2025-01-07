ca854a595d99edd23bb19b90bab3fed4da25f9fa
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2025-01-07 17:53:52 +0600

    Add files via upload

---
+++ Final/3tier_Intro/BLL/DTOs/StudentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class StudentDTO
+    {
+   
+        public int StudentID { get; set; }
+
+         public string StudentName { get; set; }
+    }
+}
