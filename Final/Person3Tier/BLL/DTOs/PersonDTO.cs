260e51b954a63204da3ed91245b52deff55782c6
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2025-01-08 10:50:32 +0600

    Add files via upload

---
+++ Final/Person3Tier/BLL/DTOs/PersonDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class PersonDTO
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Final/Person3Tier/BLL/Services/PersonService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Services
+{
+    public class PersonService
+    {
+        // AutoMapper Configuration
+        private static MapperConfiguration config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Person, PersonDTO>().ReverseMap();
+        });
+
+        private static Mapper mapper = new Mapper(config);
+
+        public static List<PersonDTO> Get()
+        {
+            var repo = new PersonRepo();
+            var data = repo.GetAll();
+            return mapper.Map<List<PersonDTO>>(data);
+        }
+
+        public static PersonDTO Get(int id)
+        {
+            var repo = new PersonRepo();
+            var data = repo.GetById(id);
+            return mapper.Map<PersonDTO>(data);
+        }
+
+        public static void Create(PersonDTO personDTO)
+        {
+            var repo = new PersonRepo();
+            var entity = mapper.Map<Person>(personDTO);
+            repo.Create(entity);
+        }
+
+        public static void Delete(int id)
+        {
+            var repo = new PersonRepo();
+            repo.Delete(id);
+        }
+    }
+}
