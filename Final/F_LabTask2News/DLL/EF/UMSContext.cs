ceb0881750196320a56e93cb6d5614d35b5c26da
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2025-01-09 11:26:16 +0600

    Add files via upload

---
+++ Final/F_LabTask2News/DLL/EF/UMSContext.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DAL.EF
+//{
+//    internal class UMSContext
+//    {
+
+//    }
+//}
+
+
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace DAL.EF
+{
+    internal class UMSContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+    }
+}
---
+++ Final/F_LabTask2News/DLL/Repos/NewsRepo.cs
@@ -0,0 +1,127 @@
+﻿using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL.Repos
+{
+    public class NewsRepo
+    {
+        private readonly UMSContext db;
+
+        public NewsRepo()
+        {
+            db = new UMSContext();
+        }
+
+     
+        public List<News> GetAll()
+        {
+            return db.News.ToList();
+        }
+
+       
+        public List<News> GetByTitle(string title)
+        {
+            return db.News.Where(n => n.Title.Contains(title)).ToList();
+        }
+
+       
+        public List<News> GetByCategory(string category)
+        {
+            return db.News.Where(n => n.Category.Contains(category)).ToList();
+        }
+
+     
+        public List<News> GetByDate(DateTime date)
+        {
+            return db.News
+                     .Where(n => DbFunctions.TruncateTime(n.Date) == date.Date)
+                     .ToList(); 
+        }
+
+    
+
+
+        public List<News> GetByDateAndCategory(DateTime date, string category)
+        {
+            return db.News
+                     .Where(n => DbFunctions.TruncateTime(n.Date) == date.Date && n.Category.Contains(category))
+                     .ToList();
+        }
+
+
+    
+
+
+
+        public List<News> GetByDateAndTitle(DateTime date, string title)
+        {
+            return db.News
+                     .Where(n => DbFunctions.TruncateTime(n.Date) == date.Date && n.Title.Contains(title))
+                     .ToList();
+        }
+
+
+        public bool Create(News news)
+        {
+            try
+            {
+                db.News.Add(news);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public bool Update(int id, News updatedNews)
+        {
+            try
+            {
+                var news = db.News.FirstOrDefault(n => n.Id == id);
+                if (news != null)
+                {
+                    news.Title = updatedNews.Title;
+                    news.Category = updatedNews.Category;
+                    news.Date = updatedNews.Date;
+
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var news = db.News.FirstOrDefault(n => n.Id == id);
+                if (news != null)
+                {
+                    db.News.Remove(news);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
