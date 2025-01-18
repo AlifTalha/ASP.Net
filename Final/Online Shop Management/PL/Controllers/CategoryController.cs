e928f52377b37e7e871e80c147b007b7b945ba29
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2025-01-18 13:20:01 +0600

    Add files via upload

--- Final/Online Shop Management/PL/Controllers/CategoryController.cs
+++ Final/Online Shop Management/PL/Controllers/CategoryController.cs
@@ -1,7 +1,44 @@
-﻿using BLL.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using BLL.Services;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
+
+//namespace TieePMS.Controllers
+//{
+//    public class CategoryController : ApiController
+//    {
+
+//        [HttpGet]
+//        [Route("api/category/all")]
+//        public HttpResponseMessage Get()
+//        {
+//            var data = CategoryService.Get();
+//            return Request.CreateResponse(HttpStatusCode.OK, data);
+//        }
+//        [HttpGet]
+//        [Route("api/category/{id}")]
+//        public HttpResponseMessage Get(int id)
+//        {
+//            var data = CategoryService.Get(id);
+//            return Request.CreateResponse(HttpStatusCode.OK, data);
+//        }
+//        [HttpGet]
+//        [Route("api/category/{id}/products")]
+//        public HttpResponseMessage GetwithProducts(int id)
+//        {
+//            var data = CategoryService.GetwithProducts(id);
+//            return Request.CreateResponse(HttpStatusCode.OK, data);
+//        }
+//    }
+//}
+
+
+
+
+using BLL.Services;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -14,9 +51,17 @@ namespace TieePMS.Controllers
         [Route("api/category/all")]
         public HttpResponseMessage Get()
         {
+            // Check for a valid token in the Authorization header
+            var authHeader = Request.Headers.Authorization;
+            if (authHeader == null || authHeader.Scheme != "Bearer" || !UserService.IsTokenValid(authHeader.Parameter))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Unauthorized access. Please log in.");
+            }
+
             var data = CategoryService.Get();
             return Request.CreateResponse(HttpStatusCode.OK, data);
         }
+
         [HttpGet]
         [Route("api/category/{id}")]
         public HttpResponseMessage Get(int id)
@@ -24,6 +69,7 @@ namespace TieePMS.Controllers
             var data = CategoryService.Get(id);
             return Request.CreateResponse(HttpStatusCode.OK, data);
         }
+
         [HttpGet]
         [Route("api/category/{id}/products")]
         public HttpResponseMessage GetwithProducts(int id)
